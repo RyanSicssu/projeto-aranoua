125e4cce4bfab651f7025c4552217b7df95f6ec4
Ryan Sicssu <sicssu21@gmail.com>
2025-09-18 01:03:02 -0400

    Atualizei header e visual moderno do site

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,17 +2,24 @@ import { useNavigate } from "react-router-dom";
 import "./Pages.css";
 
 function Login() {
-  const navigate = useNavigate();
-  return (
-    <div className="container">
-      <h1>Login</h1>
-      <input type="text" placeholder="Usuário" className="input" />
-      <input type="password" placeholder="Senha" className="input" />
-      <button onClick={() => navigate("/consulta")} className="btn">
-        Entrar
-      </button>
-    </div>
-  );
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <header>Telemedicina IFAM</header>
+            <div className="container">
+                <h1>Login</h1>
+                <input type="text" placeholder="Usuário" className="input" />
+                <input type="password" placeholder="Senha" className="input" />
+                <button onClick={() => navigate("/consulta")} className="btn">
+                    Entrar
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default Login;
+
+
+
