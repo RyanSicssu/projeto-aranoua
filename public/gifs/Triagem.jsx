98d090a6bf9d8c7db3501d00a3d5e51a782ebc60
2025002009-pb <2025002009@ifam.edu.br>
2025-09-23 14:36:55 +0000

    atualização do package

---
+++ public/gifs/Triagem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Triagem(){
+    return(
+        <div classNome="container my-4">
+        <h3 className="mb-3">Instruções de Triagem
+        <p className="text-muted">
+            siga as instruções abaixo antes da conaulta online:
+        </p>
+        </h3>
+        <div ClassName="row">
+        <div ClassName=" col-md-4 text-center">
+            <img src="/gifs/medir-pressao.gif"alt="Medir Pressão" className="img-fluid rounded">
+            <p>Medir Pressão</p>
+        </div>
+        <div ClassName="col-md-4 text-center">
+        
+
+
+
+        </div>
+        </div>
+    )
+}
